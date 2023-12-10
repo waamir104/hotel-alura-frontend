95a7ceac3afd027f25d7052fbb88c9e48bdb2f9b
waamir104 <waamirdev@gmail.com>
2023-12-10 19:43:24 -0400

    Adding the routing configurations for the app paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { AboutUsComponent } from './shared/components/about-us/about-us.component';
+import { SignUpComponent } from './shared/components/sign-up/sign-up.component';
+import { SignInComponent } from './shared/components/sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "about-us",
+    component: AboutUsComponent
+  },
+  {
+    path: "sign-up",
+    component: SignUpComponent
+  },
+  {
+    path: "sign-in",
+    component: SignInComponent
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
