f3e00e90a02c2ed8f1a9c097d7d6c6ac194cd48f
waamir104 <waamirdev@gmail.com>
2023-12-10 19:45:11 -0400

    Adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { AboutUsComponent } from './shared/components/about-us/about-us.component';
+import { CarouselComponent } from './shared/components/carousel/carousel.component';
+import { SignInComponent } from './shared/components/sign-in/sign-in.component';
+import { SignUpComponent } from './shared/components/sign-up/sign-up.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutUsComponent,
+    CarouselComponent,
+    SignInComponent,
+    SignUpComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
