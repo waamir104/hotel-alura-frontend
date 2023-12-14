5794be70a60eb47137f2cf39643342e5e1f260fb
waamir104 <waamirdev@gmail.com>
2023-12-14 10:01:58 -0500

    Adding the pagination styling and fuctionality

--- src/app/shared/components/rooms-pagination/rooms-pagination.component.ts
+++ src/app/shared/components/rooms-pagination/rooms-pagination.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
 import { Page } from 'src/app/core/domain/page';
 import { RoomService } from '../../services/room.service';
 
@@ -11,17 +11,39 @@ import { RoomService } from '../../services/room.service';
 })
 export class RoomsPaginationComponent implements OnInit {
   roomPaginationState$: Observable<{ componentState: string, componentData?: Page, error?: HttpErrorResponse }>;
+  responseSubject = new BehaviorSubject<Page>(null);
+  private currentPageSubject = new BehaviorSubject<number>(0);
+  currentPage$ = this.currentPageSubject.asObservable();
 
   constructor(private roomService: RoomService) { }
 
   ngOnInit(): void {
     this.roomPaginationState$ = this.roomService.rooms$().pipe(
       map((response: Page) => {
+        this.responseSubject.next(response);
+        this.currentPageSubject.next(response.pageable.pageNumber);
         console.log(response);
         return ({ componentState: "COMPONENT_LOADED", componentData: response })
       }),
       startWith({ componentState: "COMPONENT_LOADING" }),
       catchError((error: HttpErrorResponse) => of({ componentState: "COMPONENT_ERROR", error }))
     );
+  }
+
+  goToPage(pageNumber?: number): void {
+    this.roomPaginationState$ = this.roomService.rooms$(pageNumber).pipe(
+      map((response: Page) => {
+        this.responseSubject.next(response);
+        this.currentPageSubject.next(pageNumber);
+        console.log(response);
+        return ({ componentState: "COMPONENT_LOADED", componentData: response })
+      }),
+      startWith({ componentState: "COMPONENT_LOADED", componentData: this.responseSubject.value }),
+      catchError((error: HttpErrorResponse) => of({ componentState: "COMPONENT_ERROR", error }))
+    );
+  }
+
+  goToNextOrPreviousPage(direction?: string): void {
+    this.goToPage(direction === 'forward' ? this.currentPageSubject.value + 1 : this.currentPageSubject.value - 1);
   }
 }
