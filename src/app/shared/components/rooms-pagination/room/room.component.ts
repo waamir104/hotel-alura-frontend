08b4ebcad1378e0abcdbd592e2ccda66de90ffdb
waamir104 <waamirdev@gmail.com>
2023-12-14 08:47:26 -0500

    Styling the room component for the room pagination

--- src/app/shared/components/rooms-pagination/room/room.component.ts
+++ src/app/shared/components/rooms-pagination/room/room.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Room } from 'src/app/core/domain/room';
 
 @Component({
   selector: 'app-room',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./room.component.scss']
 })
 export class RoomComponent {
+
+  @Input() room: Room;
+
+  constructor(){}
 
 }
